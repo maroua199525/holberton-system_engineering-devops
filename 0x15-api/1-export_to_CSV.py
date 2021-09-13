ef7859e4b1047cc68ec01911dbfa10ffec8b2ed6
maroua199525 <2646@holbertonschool.com>
2021-09-13 20:50:42 +0100

    pep8 fixed

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """
-Write a Python script that, using this REST API,
-for a given employee ID,
-returns information about his/her TODO list progress
+Using what you did in the task #0,
+extend your Python script to export data in the CSV format
 """
 import requests
 import sys
@@ -10,17 +9,19 @@ import csv
 
 
 def employee_csv():
-    
+
     employeeId = sys.argv[1]
     url_user = "https://jsonplaceholder.typicode.com/users/" + employeeId
     response = requests.get(url_user).json()
     EMPLOYEE_NAME = response.get('name')
-    url_todo = "https://jsonplaceholder.typicode.com/users/" + employeeId + '/todos'
+    url_todo = ("https://jsonplaceholder.typicode.com/users/" + employeeId +
+                '/todos')
     response1 = requests.get(url_todo).json()
     with open("{}.csv".format(employeeId), mode='w') as csv_file:
         writer = csv.writer(csv_file, quoting=csv.QUOTE_ALL)
         for user in response1:
-            writer.writerow([employeeId, EMPLOYEE_NAME, user.get("completed"), user.get("title")])
+            writer.writerow([employeeId, EMPLOYEE_NAME, user.get("completed"),
+                            user.get("title")])
 
 if __name__ == '__main__':
     employee_csv()
