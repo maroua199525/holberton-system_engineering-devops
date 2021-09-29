66ea670090afc280963fd0c8349c268705226aee
maroua199525 <2646@holbertonschool.com>
2021-09-29 19:23:58 +0100

    add headers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,8 +7,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    user = {"User-Agent": "Maroua-Alaya"}
     request = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit))
+                           .format(subreddit), headers=user)
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
     else:
